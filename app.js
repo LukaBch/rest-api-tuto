e6650bf7b40c22172eb6d8d9633098f2bcc19d50
LukaBch <lucas.banchereau@gmail.com>
2022-08-16 15:36:11 +0200

    app using new route for Post

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = express();
 
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
 require("dotenv/config");
+
+const postsRoute = require("./routes/posts.js");
+
+app.use("/posts", postsRoute);
 
 // ROUTES
 
 app.get("/", (req, res) => {
   res.send("We are on home!");
+});
+
+app.get("/posts", (req, res) => {
+  res.send("We are on posts!");
 });
 
 mongoose.connect(process.env.DB_CONNECTION, () => {
